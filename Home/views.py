6e158b1227b38d237d4a1a9f1180830ae8f84bea
mersad mir <mersad.mir96@gmail.com>
2023-07-09 16:28:43 +0330

    index(home) models are fixed

--- Home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Article
-
-def index(request):
-    articles = Article.objects.all()
-    return render(request, 'Home/index.html', context={"articles": articles})
-
-
-
